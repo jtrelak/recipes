3e5b191057f43e12e623916c1056414a0c585345
Jaroslaw Trelak <jtrelak@vewd.com>
2020-10-12 17:13:03 +0200

    docker minecraft: added gfx acceleration

--- docker/minecraft/run.sh
+++ docker/minecraft/run.sh
@@ -1,2 +1,7 @@
 docker build -t minecraft_gui .
-docker run --network=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -h $HOSTNAME -v $HOME/.Xauthority:/home/jtrelak/.Xauthority minecraft_gui
+docker run \
+    --device=/dev/dri \
+    --group-add video \
+    --network=host \
+    --volume=/tmp/.X11-unix:/tmp/.X11-unix \
+    --env="DISPLAY=$DISPLAY" -h $HOSTNAME -v $HOME/.Xauthority:/home/jtrelak/.Xauthority minecraft_gui
